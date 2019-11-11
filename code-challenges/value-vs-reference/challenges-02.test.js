6fd583d704f43cde9dfeb230bf6aced12db55a1c
Nsuliman <nsuliman88@gmail.com>
2019-11-11 23:26:47 +0200

    4th challenge

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -66,6 +66,9 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach(element => {
+      element.isAuthor = true;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
