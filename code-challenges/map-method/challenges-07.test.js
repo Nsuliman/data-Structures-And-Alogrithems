e419d5be9999dc4061b71b1d1cec25fd296e047b
Nsuliman <nsuliman88@gmail.com>
2019-11-29 03:42:50 +0200

    1st challenge Done

--- code-challenges/map-method/challenges-07.test.js
+++ code-challenges/map-method/challenges-07.test.js
@@ -11,12 +11,13 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 ------------------------------------------------------------------------------------------------ */
 
 const forLoopTwoToThe = (arr) => {
-  // Solution code here...
-  let powerArray = arr.map ((value,idx) =>
-  {
-    return value * 2 ;
-  });
-//   return powerArray;
+  // Solution code here..
+
+  let power2array = [];
+  for (let i=0;i<arr.length;i++) {
+    power2array.push(Math.pow(2,arr[i]));  /// to power of a number 
+  }
+  return power2array;
 };
 
 /* ------------------------------------------------------------------------------------------------
