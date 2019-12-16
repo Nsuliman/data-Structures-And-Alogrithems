cb0b575ead9f13683d3ef60837bc74b1e78db6cf
Nsuliman <nsuliman88@gmail.com>
2019-12-16 18:51:03 +0200

    3rd challenge Done

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -51,7 +51,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
-  // obj.yearBorn = year;
+  obj.yearBorn = year;      // Soo easy 
 };
 
 /* ------------------------------------------------------------------------------------------------
