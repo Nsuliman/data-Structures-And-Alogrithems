379900359ea9b515ffe63147cf194d615afe5180
Nsuliman <nsuliman88@gmail.com>
2019-12-16 18:57:14 +0200

    5th challenge Done, already I solved it on first time by myself all challenges

--- code-challenges/value-vs-reference/challenges-02.test.js
+++ code-challenges/value-vs-reference/challenges-02.test.js
@@ -96,11 +96,10 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
-  // arr2.forEach( element => 
-  //   {
-  //       arr1.push(element);
-  //   });
-
+  arr2.forEach( element => 
+    {
+        arr1.push(element);
+    });
 };
 
 /* ------------------------------------------------------------------------------------------------
