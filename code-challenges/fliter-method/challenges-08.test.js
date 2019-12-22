c2e818f5dae976f282b1ded8e0cfb1adb3d89426
Nsuliman <nsuliman88@gmail.com>
2019-12-22 14:40:15 +0200

    3rd challenge Done

--- code-challenges/fliter-method/challenges-08.test.js
+++ code-challenges/fliter-method/challenges-08.test.js
@@ -39,7 +39,6 @@ const filterStringsWithVowels = (arr) => {
   return strVowels;
 };
 
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
