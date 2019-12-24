b7a7079fdcce2faf96f1292318860c2fc2634a84
Nsuliman <nsuliman88@gmail.com>
2019-12-25 00:46:08 +0200

    5th challenge Done

--- code-challenges/twoDimArrays/challenges-12.test.js
+++ code-challenges/twoDimArrays/challenges-12.test.js
@@ -143,11 +143,27 @@ CHALLENGE 5
 
 Write a function named calculateProduct that takes in a two-dimensional array of numbers, multiplies all of the numbers in each array, and returns the final product. This function should work for any number of inner arrays.
 
-For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
+For example, the following input returns a product of 720: 
+[[1,2], 
+[3,4], 
+[5,6]]
 ------------------------------------------------------------------------------------------------ */
 
 const calculateProduct = (numbers) => {
   // Solution code here...
+
+  let multiplySum = 1;
+  for (let i=0;i<numbers.length;i++)
+  {
+    // console.log('i : ', i);
+    for (let j=0;j<numbers[i].length;j++)
+    {
+      // console.log('j : ', j);
+      multiplySum *= numbers[i][j];
+    }
+  }
+  // console.log('multiplySum : ', multiplySum);
+  return multiplySum;
 };
 
 /* ------------------------------------------------------------------------------------------------
