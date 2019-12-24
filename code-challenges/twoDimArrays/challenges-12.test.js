aa96af761c0db610f6e9afe2067531b51b8a3bb9
Nsuliman <nsuliman88@gmail.com>
2019-12-25 00:39:01 +0200

    4th challenge Done

--- code-challenges/twoDimArrays/challenges-12.test.js
+++ code-challenges/twoDimArrays/challenges-12.test.js
@@ -128,15 +128,14 @@ The top row of the board is considered row zero and row numbers increase as they
 
 const battleship = (board, row, col) => {
   //  Solution code here...
-  // if (board[row][col]=== '#' )
-  // {
-  //     return 'hit';
-  // }
-  // else
-  // {
-  //   return 'miss';
-
-  // }
+  if (board[row][col]=== '#' )
+  {
+      return 'hit';
+  }
+  else
+  {
+    return 'miss';
+  }
 };
 
 /* ------------------------------------------------------------------------------------------------
