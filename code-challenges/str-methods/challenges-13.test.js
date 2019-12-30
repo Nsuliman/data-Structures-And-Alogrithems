90f9f92467ea6885dc713477ac488ba20fe89aad
Nsuliman <nsuliman88@gmail.com>
2019-12-30 13:17:06 +0200

    7th challenges Done

--- code-challenges/str-methods/challenges-13.test.js
+++ code-challenges/str-methods/challenges-13.test.js
@@ -108,6 +108,8 @@ Write a function named findEvery that takes in an array of strings, along with a
 
 const findEvery = (arr, target) => {
   // Solution code here...
+  return arr.every( element => element.includes(target) );
+
 };
 
 /* ------------------------------------------------------------------------------------------------
